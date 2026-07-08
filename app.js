efcf56535e7e0c9e94d7ad2497a65606863f1824
Антон <mikolenko.anton1@gmail.com>
2026-07-08 14:04:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import {
   formatCurrency,
   getDefaultInputs,
   roundMoney
-} from "./calculator.js?v=39";
+} from "./calculator.js?v=40";
 
 const STORAGE_KEY_PREFIX = "zp-2-2-calculator-inputs";
 const form = document.querySelector("#calculatorForm");
